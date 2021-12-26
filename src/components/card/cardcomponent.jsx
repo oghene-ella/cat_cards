afcd398fc0f6e79e38d5934ac3cf30790da0675a
Emmanuella Oghenekaro <oghenekaro57@gmail.com>
2021-12-26 19:00:57 +0100

    edited the card components

--- src/components/card/cardcomponent.jsx
+++ src/components/card/cardcomponent.jsx
@@ -5,10 +5,10 @@ import './cardcomponent.css';
 export const Card = props => (
   <div className='card-container'>
     <img
-      alt='monster'
+      alt='cat'
       src={`https://robohash.org/${props.cat.id}?set=set4&`}
     />
     <h2> {props.cat.username} </h2>
-    <p>Origin <b><br/>{props.cat.address.city}</b> </p>
+    <p>Origin <b><br/>{props.cat.address.city}<img src={`https://robohash.org/${props.cat.id}?set=set4&size=15x15`}/></b> </p>
   </div>
 );
